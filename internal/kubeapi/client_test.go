5368cdc955b0a4b3b88dc70c4c9d5a74c520df1f
YangYuS8 <YangYuS8@163.com>
2026-07-16 14:12:49 +0800

    test: import Kubernetes runtime objects for fake clientset

--- internal/kubeapi/client_test.go
+++ internal/kubeapi/client_test.go
@@ -12,6 +12,7 @@ import (
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/apimachinery/pkg/version"
